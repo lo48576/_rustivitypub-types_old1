f31652330b20590db1bfa588b964dd8dc4309df8
YOSHIOKA Takuma <lo48576@hard-wi.red>
2018-04-30 19:27:55 +0900

    Re-export useful types from `base` module

--- src/base/mod.rs
+++ src/base/mod.rs
@@ -1,4 +1,7 @@
 //! Basic types.
 
+pub use self::iri::{Iri, IriStr, IriString};
+pub use self::non_empty_digits::{NonEmptyDigitsStr, NonEmptyDigitsString};
+
 pub mod iri;
 pub mod non_empty_digits;
